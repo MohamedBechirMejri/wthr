d1b876e68b97060b8caf6b45675ef9c3e9d4a8fb
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-27 12:13:09 +0100

    add api call functions

--- src/js/grabWeatherData.js
+++ src/js/grabWeatherData.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-console */
 import axios from 'axios';
 
-const grabWeatherData = async (location) => {
+const grabWeatherData = async (lat, lon) => {
+  const weatherData = await axios.get(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=c8a1733571c211a516b796af5a8acda7`,
+  );
+  console.log(weatherData.data);
+};
+const grabWeatherDataOldApi = async (location) => {
   const weatherData = await axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c8a1733571c211a516b796af5a8acda7`,
   );
   console.log(weatherData.data);
-  /* TODO: #1 switch to oneCall api
-  const weatherData2 = await axios.get(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=9&lon=34&appid=c8a1733571c211a516b796af5a8acda7`,
-  );
-  */
 };
-
-export default grabWeatherData;
+export { grabWeatherDataOldApi, grabWeatherData as default };
