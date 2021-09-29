d1782822ec7f45d5e6e5f99865676be60858797e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-29 14:33:48 +0100

    add city name from coords

--- src/js/organizeData.js
+++ src/js/organizeData.js
@@ -1,21 +1,36 @@
-import grabWeatherData, { grabNewData } from './grabWeatherData';
+import axios from 'axios';
+import grabWeatherData, {
+    grabNewData
+} from './grabWeatherData';
 import showDataToUser from './showDataToUser';
 
 const organizeData = async (location) => {
-  const oldData = await grabWeatherData(location);
-  const newData = await grabNewData(oldData.coord.lat, oldData.coord.lon);
+    const oldData = await grabWeatherData(location);
+    const newData = await grabNewData(oldData.coord.lat, oldData.coord.lon);
+    let city = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/reverse?lat=${oldData.coord.lat}&lon=${oldData.coord.lon}&appid=c8a1733571c211a516b796af5a8acda7`,
+    );
 
-  const { main } = oldData;
-  const { current, daily, hourly, minutely } = newData;
+    const {
+        main
+    } = oldData;
+    const {
+        current,
+        daily,
+        hourly,
+        minutely
+    } = newData;
+    city = city.data[0].name;
 
-  const data = {
-    main,
-    current,
-    daily,
-    hourly,
-    minutely,
-  };
+    const data = {
+        main,
+        current,
+        daily,
+        hourly,
+        minutely,
+        city,
+    };
 
-  showDataToUser(data);
+    showDataToUser(data);
 };
 export default organizeData;
