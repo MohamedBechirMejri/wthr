429f5bcf0f2a658935d42916aa80230d49645927
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-29 05:58:03 +0100

    refactor function to use both input methods

--- src/js/grabWeatherData.js
+++ src/js/grabWeatherData.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-console */
 import axios from 'axios';
 
-const grabWeatherData = async (lat, lon) => {
+const grabWeatherData = async (location) => {
+  // location = lat=${lat}&lon=${lon} or q=${location}
+
   const weatherData = await axios.get(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=c8a1733571c211a516b796af5a8acda7`,
+    `https://api.openweathermap.org/data/2.5/weather?${location}&appid=c8a1733571c211a516b796af5a8acda7`,
   );
-  console.log(weatherData.data);
+  return weatherData.data;
 };
-const grabWeatherDataOldApi = async (location) => {
-  const weatherData = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c8a1733571c211a516b796af5a8acda7`,
-  );
-  console.log(weatherData.data);
-};
-export { grabWeatherDataOldApi, grabWeatherData as default };
+export { grabWeatherData as default };
