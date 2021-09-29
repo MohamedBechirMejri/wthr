94cbc9d3b3650f6ebb570735cbff9163a63c9ff9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-29 14:53:52 +0100

    show feels like

--- src/js/showDataToUser.js
+++ src/js/showDataToUser.js
@@ -1,10 +1,13 @@
 const showDataToUser = (data) => {
   const temp = document.getElementById('temp-value');
   const location = document.getElementById('location');
+  const feelsLike = document.getElementById('feels-like');
 
-    temp.innerHTML = (data.main.temp - 273.15).toFixed(0);
-    location.innerHTML = data.city
-    console.log(data);
+  console.log(data);
+
+  temp.innerHTML = (data.main.temp - 273.15).toFixed(0);
+  location.innerHTML = data.city;
+  feelsLike.innerHTML = (data.main.feels_like - 273.15).toFixed(0);
 };
 
 export default showDataToUser;
