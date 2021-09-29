3cc990e14df8c79dfda7ddec2ce562c9b1285d97
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-29 14:35:54 +0100

    show city on page

--- src/js/showDataToUser.js
+++ src/js/showDataToUser.js
@@ -3,7 +3,7 @@ const showDataToUser = (data) => {
   const location = document.getElementById('location');
 
     temp.innerHTML = (data.main.temp - 273.15).toFixed(0);
-    location.innerHTML = data.main
+    location.innerHTML = data.city
     console.log(data);
 };
 
