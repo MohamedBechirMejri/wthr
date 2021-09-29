d0fa53d020b823c195aed6705f0f32f9ac6df2fd
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-29 16:23:34 +0100

    gonna be too much code so i'm redoing it

--- src/js/showDataToUser.js
+++ src/js/showDataToUser.js
@@ -1,16 +1,8 @@
 const showDataToUser = (data) => {
-  const temp = document.getElementById('temp-value');
-  const location = document.getElementById('location');
-  const feelsLike = document.getElementById('feels-like');
-  const clouds = document.getElementById('clouds');
 
   // eslint-disable-next-line no-console
   console.log(data);
 
-  temp.innerHTML = (data.main.temp - 273.15).toFixed(0);
-  location.innerHTML = data.city;
-  feelsLike.innerHTML = (data.main.feels_like - 273.15).toFixed(0);
-  clouds.innerHTML = data.current.weather[0].description;
 };
 
 export default showDataToUser;
