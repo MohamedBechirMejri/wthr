d3a71d4b90253b1d040134d6542e46217c4f52ad
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-30 05:52:24 +0100

    new api blocked

--- src/js/organizeData.js
+++ src/js/organizeData.js
@@ -1,38 +1,25 @@
-import axios from 'axios';
-import grabWeatherData, {
-    grabNewData
-} from './grabWeatherData';
+import grabWeatherData from './grabWeatherData';
 import showDataToUser from './showDataToUser';
 
 const organizeData = async (location) => {
-    const oldData = await grabWeatherData(location);
-    const newData = await grabNewData(oldData.coord.lat, oldData.coord.lon);
-    let city = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/reverse?lat=${oldData.coord.lat}&lon=${oldData.coord.lon}&appid=c8a1733571c211a516b796af5a8acda7`,
-    );
+  const oldData = await grabWeatherData(location);
+  // const newData = await grabNewData(oldData.coord.lat, oldData.coord.lon);
+  // let city = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${oldData.coord.lat}&lon=${oldData.coord.lon}&appid=c8a1733571c211a516b796af5a8acda7`,);
 
-    const {
-        main
-    } = oldData;
-    const {
-        current,
-        daily,
-        hourly
-    } = newData;
-    const {
-        country
-    } = city.data[0];
-    city = city.data[0].name;
+  const { main } = oldData;
+  // const { current, daily, hourly } = newData;
+  // const { country } = city.data[0];
+  // city = city.data[0].name;
 
-    const data = {
-        main,
-        current,
-        daily,
-        hourly,
-        country,
-        city,
-    };
+  const data = {
+    main,
+    // current,
+    // daily,
+    // hourly,
+    // country,
+    // city,
+  };
 
-    showDataToUser(data);
+  showDataToUser(data);
 };
 export default organizeData;
