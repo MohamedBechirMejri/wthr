3b3590bca2262ddf9ba6ac5a988cb862f8b587d5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-30 05:52:55 +0100

    tweaks

--- src/js/showDataToUser.js
+++ src/js/showDataToUser.js
@@ -4,19 +4,27 @@ const showDataToUser = (data) => {
   currentWeather.innerHTML = `
         <p id="date">${new Date()}</p>
         <h3 id="location">
-          <span id="city">${data.city}</span>, <span id="country">${data.country}</span>
+          <span id="city">${data.city}</span>, <span id="country">${
+    data.country
+  }</span>
         </h3>
+        <div class="temp-plus-data">
         <div class="temp">
-          <h2 id="temp" class="temp-value">${data.current.temp}°</h2>
+          <h2 id="temp" class="temp-value">${data.current.temp}°K</h2>
           <div id="units">
             <span id="kelvin" class="unit">K</span>
             <span id="fahrenheit" class="unit">F</span>
             <span id="celsius" class="unit">C</span>
           </div>
         </div>
-        <h4 class="feels-like">feels like <span id="feels-like" class="temp-value">${data.current.feels_like}°K</span></h4>
+        <h4 class="feels-like">feels like <span id="feels-like" class="temp-value">${
+          data.current.feels_like
+        }°K</span></h4>
         <h4 id="clouds">${data.current.weather[0].description}</h4>
-        <h4 class="humidity">humidity <span id="humidity">${data.current.humidity}</span></h4>
+        <h4 class="humidity">humidity <span id="humidity">${
+          data.current.humidity
+        }</span></h4>
+        </div>
         `;
   // eslint-disable-next-line no-console
   console.log(data);
