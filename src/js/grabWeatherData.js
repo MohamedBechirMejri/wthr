2ad84d98e78532ef630c41293148fec7a000df3c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-30 13:57:22 +0100

    add test object to reduce requests

--- src/js/grabWeatherData.js
+++ src/js/grabWeatherData.js
@@ -4,10 +4,48 @@ import axios from 'axios';
 const grabWeatherData = async (location) => {
   // location = lat=${lat}&lon=${lon} or q=${location}
 
-  const weatherData = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?${location}&appid=c8a1733571c211a516b796af5a8acda7`,
-  );
-  return weatherData.data;
+  /*
+      const weatherData = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?${location}&appid=c8a1733571c211a516b796af5a8acda7`,
+      );
+        */
+
+  const weatherData = {
+    main: {
+      temp: 301.03,
+      feels_like: 300.85,
+      temp_min: 300.03,
+      temp_max: 301.03,
+      pressure: 1022,
+      humidity: 42,
+    },
+    name: 'Manouba',
+    sys: {
+      type: 1,
+      id: 1194,
+      country: 'TN',
+      sunrise: 1632978833,
+      sunset: 1633021501,
+    },
+    weather: [
+      {
+        id: 802,
+        main: 'Clouds',
+        description: 'scattered clouds',
+        icon: '03d',
+      },
+    ],
+    wind: {
+      speed: 6.69,
+      deg: 310,
+    },
+    visibility: 10000,
+  };
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  // return weatherData.data;
+    return weatherData;
+    
 };
 const grabNewData = async (lat, lon) => {
   const weatherData = await axios.get(
