78920d9ff2fc07f727c8ca447ce7b2abd0479280
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-30 11:27:49 +0100

    add more data

--- src/js/showDataToUser.js
+++ src/js/showDataToUser.js
@@ -4,26 +4,37 @@ const showDataToUser = (data) => {
   currentWeather.innerHTML = `
         <p id="date">${new Date()}</p>
         <h3 id="location">
-          <span id="city">${data.name}</span>, <span id="country">${
-    data.sys.country
-  }</span>
+          <span id="city">${data.name}</span>,
+          <span id="country">${data.sys.country}</span>
         </h3>
         <div class="temp-plus-data">
-        <div class="temp">
-          <h2 id="temp" class="temp-value">${data.main.temp}°K</h2>
-          <div id="units">
-            <span id="kelvin" class="unit">K</span>
-            <span id="fahrenheit" class="unit">F</span>
-            <span id="celsius" class="unit">C</span>
+          <div class="temp">
+            <h2 id="temp" class="temp-value">${data.main.temp}°K</h2>
+            <div id="units">
+              <span id="kelvin" class="unit">K</span>
+              <span id="fahrenheit" class="unit">F</span>
+              <span id="celsius" class="unit">C</span>
+            </div>
           </div>
-        </div>
-        <h4 class="feels-like">feels like <span id="feels-like" class="temp-value">${
-          data.main.feels_like
-        }°K</span></h4>
-        <h4 id="clouds">${data.weather[0].description}</h4>
-        <h4 class="humidity">humidity <span id="humidity">${
-          data.main.humidity
-        }</span></h4>
+          <div class="other-data">
+            <div id="other-data">
+              <h4 class="feels-like">
+                feels like
+                <span id="feels-like" class="temp-value"
+                  >${data.main.feels_like}°K</span
+                >
+              </h4>
+              <h4 id="clouds">${data.weather[0].description}</h4>
+              <h4 class="humidity">
+                humidity <span id="humidity">${data.main.humidity}</span>%
+              </h4>
+            </div>
+            <div id="extra-data">
+              <h4 id="wind">${data.wind.speed.toFixed(1)} m/s</h4>
+              <h4 id="visibility">${(data.visibility / 1000).toFixed(1)}Km</h4>
+              <h4 id="pressure">${data.main.pressure}hPa</h4>
+            </div>
+          </div>
         </div>
         `;
   // eslint-disable-next-line no-console
