a31a7995b8f862c87e13ae5fb9f0a67b837a4548
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-30 06:08:25 +0100

    use old api

--- src/js/showDataToUser.js
+++ src/js/showDataToUser.js
@@ -4,13 +4,13 @@ const showDataToUser = (data) => {
   currentWeather.innerHTML = `
         <p id="date">${new Date()}</p>
         <h3 id="location">
-          <span id="city">${data.city}</span>, <span id="country">${
-    data.country
+          <span id="city">${data.name}</span>, <span id="country">${
+    data.sys.country
   }</span>
         </h3>
         <div class="temp-plus-data">
         <div class="temp">
-          <h2 id="temp" class="temp-value">${data.current.temp}°K</h2>
+          <h2 id="temp" class="temp-value">${data.main.temp}°K</h2>
           <div id="units">
             <span id="kelvin" class="unit">K</span>
             <span id="fahrenheit" class="unit">F</span>
@@ -18,11 +18,11 @@ const showDataToUser = (data) => {
           </div>
         </div>
         <h4 class="feels-like">feels like <span id="feels-like" class="temp-value">${
-          data.current.feels_like
+          data.main.feels_like
         }°K</span></h4>
-        <h4 id="clouds">${data.current.weather[0].description}</h4>
+        <h4 id="clouds">${data.weather[0].description}</h4>
         <h4 class="humidity">humidity <span id="humidity">${
-          data.current.humidity
+          data.main.humidity
         }</span></h4>
         </div>
         `;
