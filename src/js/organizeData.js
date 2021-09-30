0b9362566f7144d44daf98caa8b0e78249ceac91
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-09-30 06:08:09 +0100

    use olds api

--- src/js/organizeData.js
+++ src/js/organizeData.js
@@ -6,13 +6,18 @@ const organizeData = async (location) => {
   // const newData = await grabNewData(oldData.coord.lat, oldData.coord.lon);
   // let city = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${oldData.coord.lat}&lon=${oldData.coord.lon}&appid=c8a1733571c211a516b796af5a8acda7`,);
 
-  const { main } = oldData;
+  const { main, name, sys, weather, wind, visibility } = oldData;
   // const { current, daily, hourly } = newData;
   // const { country } = city.data[0];
   // city = city.data[0].name;
 
   const data = {
     main,
+    name,
+    sys,
+    weather,
+    wind,
+    visibility,
     // current,
     // daily,
     // hourly,
