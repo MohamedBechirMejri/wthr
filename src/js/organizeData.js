ffec942185439d05c9d35bc439fa0a6d3efe8b34
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-01 07:57:32 +0100

    add comments to test

--- src/js/organizeData.js
+++ src/js/organizeData.js
@@ -3,7 +3,7 @@ import showDataToUser from './showDataToUser';
 
 const organizeData = async (location) => {
   const oldData = await grabWeatherData(location);
-  /**/ const newData = await grabNewData(oldData.coord.lat, oldData.coord.lon);
+  /**/ const newData = await grabNewData(/* oldData.coord.lat, oldData.coord.lon */);
   // let city = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${oldData.coord.lat}&lon=${oldData.coord.lon}&appid=c8a1733571c211a516b796af5a8acda7`,);
 
   const { main, name, sys, weather, wind, visibility } = oldData;
