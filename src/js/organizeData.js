1c9e4bad1965e5640c25554c65353a32505bb7a7
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-01 07:55:25 +0100

    start work on daily data

--- src/js/organizeData.js
+++ src/js/organizeData.js
@@ -1,13 +1,13 @@
-import grabWeatherData from './grabWeatherData';
+import grabWeatherData, { grabNewData } from './grabWeatherData';
 import showDataToUser from './showDataToUser';
 
 const organizeData = async (location) => {
   const oldData = await grabWeatherData(location);
-  // const newData = await grabNewData(oldData.coord.lat, oldData.coord.lon);
+  /**/ const newData = await grabNewData(oldData.coord.lat, oldData.coord.lon);
   // let city = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${oldData.coord.lat}&lon=${oldData.coord.lon}&appid=c8a1733571c211a516b796af5a8acda7`,);
 
   const { main, name, sys, weather, wind, visibility } = oldData;
-  // const { current, daily, hourly } = newData;
+  /**/ const { /* current, */ daily, hourly } = newData;
   // const { country } = city.data[0];
   // city = city.data[0].name;
 
@@ -19,8 +19,8 @@ const organizeData = async (location) => {
     wind,
     visibility,
     // current,
-    // daily,
-    // hourly,
+    /**/ daily,
+    /**/ hourly,
     // country,
     // city,
   };
