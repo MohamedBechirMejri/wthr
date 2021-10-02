f4db5390dca8a1d8439204fbc9efb7b083c50cff
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-02 16:59:50 +0100

    listen to search queries

--- src/js/eventListeners.js
+++ src/js/eventListeners.js
@@ -1,4 +1,5 @@
 import getCurrentLocation from './getCurrentLocation';
+import organizeData from './organizeData';
 
 const listenToLocateButton = () => {
   const locateButton = document.getElementById('auto-locate');
@@ -7,8 +8,18 @@ const listenToLocateButton = () => {
   });
 };
 
+const listenToSearchButton = () => {
+  const input = document.querySelector('input');
+  const submitButton = document.getElementById('submit');
+
+  submitButton.addEventListener('click', () => {
+    organizeData(`q=${input.value}`);
+  });
+};
+
 const listen = () => {
   listenToLocateButton();
+  listenToSearchButton();
 };
 
 export default listen;
