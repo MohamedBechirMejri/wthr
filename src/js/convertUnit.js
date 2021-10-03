f68205f25ae3e7701daeaed41da8d0b44891d51c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-10-03 08:28:01 +0100

    fix function

--- src/js/convertUnit.js
+++ src/js/convertUnit.js
@@ -1,15 +1,29 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable default-case */
-const convertUnit = (value, unit) => {
-  let u;
-  const K = value;
-  const C = K - 273.15;
-  const F = (C * 9) / 5 + 32;
-
-  unit === 'C' ? (u = C) : unit === 'F' ? (u = F) : (u = K);
-
-  return u;
+const convertUnit = (value, from, to) => {
+  value = +value;
+  if (from === 'K') {
+    to === 'C'
+      ? (value = (value - 273.15).toFixed(0))
+      : to === 'F'
+      ? (value = (((value - 273.15) * 9) / 5 + 32).toFixed(0))
+      : '';
+  } else if (from === 'C') {
+    to === 'K'
+      ? (value = (value + 273.15).toFixed(0))
+      : to === 'F'
+      ? (value = ((value * 9) / 5 + 32).toFixed(0))
+      : '';
+  } else {
+    to === 'C'
+      ? (value = (((value - 32) * 5) / 9).toFixed(0))
+      : to === 'K'
+      ? (value = (((value - 32) * 5) / 9 + 273.15).toFixed(0))
+      : '';
+  }
+  return `${value}°${to}`;
 };
 
 export default convertUnit;
